2e9fab25b91bd119e9c7b9bdc5b04a570aecffe3
xgian20 <162388975+xgian20@users.noreply.github.com>
2024-04-25 23:41:25 +0000

    finished project

---
+++ src/js/component/Deck.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from './Card';
+const Deck = () => {
+    return (
+        <>
+            <Card />
+        </>
+    );
+}
+
+ export default Deck;
---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <div className="container-fluid text-bg-dark" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '150px', fontSize: '30px', marginTop: '25px', textAlign: 'center'}}>
+                Copyright © Your Website 2018
+            </div>
+        </>
+    );
+}
+
+ export default Footer;
